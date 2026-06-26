b62b204d257f0f18aad61c8f4194a353594f19e3
th1agOx <thdarochabp@gmail.com>
2026-06-25 21:47:23 -0300

    Estruturação do backend em java

---
+++ chatbot-api/src/main/java/br/com/chatbot/chatbot_api/ChatbotApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.chatbot.chatbot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotApiApplication.class, args);
+	}
+
+}
---
+++ chatbot-api/src/test/java/br/com/chatbot/chatbot_api/ChatbotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chatbot.chatbot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
